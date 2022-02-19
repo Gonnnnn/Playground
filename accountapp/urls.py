08265089aee5fe24197b75c5feeb41db0fcc3969
Gonnnnn <geonkim@yonsei.ac.kr>
2022-02-19 23:15:47 +0900

    Initial commit

---
+++ accountapp/urls.py
@@ -0,0 +1,26 @@
+"""playground URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import *
+
+# 127:00..../account/... 와 같이 접근하는 것은 번거로운 일.
+# app_name:url_name으로 위의 주소를 반환시켜주는 함수가 존재한다. 그래서 app name등을 지정해주는 것
+
+app_name = 'accountapp'
+
+urlpatterns = [
+    path('', hello_world, name='hello_world'),
+]
